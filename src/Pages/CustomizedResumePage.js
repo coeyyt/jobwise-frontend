b4ca1f25217e21402b470bc90d8c1ee9a2a43876
coeyyt <coeytky@gmail.com>
2023-11-08 14:06:15 +0800

    user can see their job application status

--- src/Pages/CustomizedResumePage.js
+++ src/Pages/CustomizedResumePage.js
@@ -13,6 +13,71 @@
 //   const { isAuthenticated, loginWithRedirect } = useAuth0();
 //   const [resumeContent, setResumeContent] = useState("");
 //   const [jdFormHistory, setJdFormHistory] = useState([]);
+//   const { getAccessTokenSilently, user } = useAuth0();
+//   const [selectedJobApplication, setSelectedJobApplication] = useState(null);
+//   const [applicationStatus, setApplicationStatus] = useState({});
+//   const applicationStatusOptions = [
+//     "Draft",
+//     "Submitted",
+//     "Under Review",
+//     "Interview",
+//     "Offer Extended",
+//     "Accepted Offer",
+//     "Rejected",
+//     "Withdrawn",
+//   ];
+
+//   // Function to fetch the current status
+//   const fetchApplicationStatus = async (jdFormId) => {
+//     if (!user || !jdFormId) {
+//       console.error("User or jdFormId is undefined. Skipping fetch.");
+//       return;
+//     }
+//     console.log(
+//       `Fetching status for job application ID: ${jdFormId} and user: ${user.sub}`
+//     );
+
+//     try {
+//       const statusResponse = await axios.get(
+//         `${BACKEND_URL}/status/getStatus/${jdFormId}/${user.sub}`
+//       );
+//       console.log(`Status response:`, statusResponse);
+
+//       if (statusResponse.status === 200) {
+//         setApplicationStatus((prevStatus) => ({
+//           ...prevStatus,
+//           [jdFormId]: statusResponse.data.application.status,
+//         }));
+//       }
+//     } catch (error) {
+//       console.error("Error fetching status:", error);
+//     }
+//   };
+
+//   // Function to update the status
+//   const updateApplicationStatus = async () => {
+//     if (!selectedJobApplication) return;
+
+//     try {
+//       const token = await getAccessTokenSilently();
+//       await axios.post(
+//         `${BACKEND_URL}/status/updateStatus`,
+//         {
+//           status: applicationStatus,
+//           job_application_id: selectedJobApplication.id,
+//           user_auth0_user_id: user.sub,
+//         },
+//         {
+//           headers: {
+//             Authorization: `Bearer ${token}`,
+//           },
+//         }
+//       );
+//       // Handle successful update (e.g., fetch updated JD forms or show a message)
+//     } catch (error) {
+//       console.error("Error updating application status:", error);
+//     }
+//   };
 
 //   const fetchCustomizedResume = async ({ jdFormId }) => {
 //     if (!jdFormId) {
@@ -37,11 +102,20 @@
 //   };
 
 //   const fetchJDForms = async () => {
-//     try {
-//       const history = await axios.get(`${BACKEND_URL}/jobapplications`);
-//       if (history.status === 200) {
-//         console.log("Fetched JDForms:", history.data);
+//     if (!isAuthenticated) return;
 
+//     try {
+//       const token = await getAccessTokenSilently();
+//       const history = await axios.get(
+//         `${BACKEND_URL}/jobapplications/${user.sub}`,
+//         {
+//           headers: {
+//             Authorization: `Bearer ${token}`,
+//           },
+//         }
+//       );
+
+//       if (history.status === 200) {
 //         setJdFormHistory(history.data);
 //       }
 //     } catch (error) {
@@ -51,7 +125,7 @@
 
 //   useEffect(() => {
 //     fetchJDForms();
-//   }, []);
+//   }, [isAuthenticated]); // Re-fetch when authentication status changes
 
 //   useEffect(() => {
 //     if (isFormVisible && !isAuthenticated) {
@@ -89,11 +163,17 @@
 //           {jdFormHistory.map((jdForm) => (
 //             <li
 //               key={jdForm.id} // Using the 'id' property for the key
-//               onClick={() => fetchCustomizedResume({ jdFormId: jdForm.id })}
+//               onClick={() => {
+//                 fetchCustomizedResume({ jdFormId: jdForm.id });
+//                 fetchApplicationStatus(jdForm.id);
+//                 setSelectedJobApplication(jdForm);
+//               }}
 //               style={{ cursor: "pointer" }}
 //             >
 //               <strong>Company:</strong> {jdForm.company_name} <br />
-//               <strong>Title:</strong> {jdForm.job_title}
+//               <strong>Title:</strong> {jdForm.job_title} <br />
+//               <strong>Status:</strong>{" "}
+//               {applicationStatus[jdForm.id] || "Not set"}
 //             </li>
 //           ))}
 //         </ul>
@@ -103,15 +183,25 @@
 //         </nav>
 //       </section>
 //       <section className="main">
-//         <h1>JOBWISE</h1>
+//         {selectedJobApplication && (
+//           <div>
+//             <h2>Update Application Status</h2>
+//             <select
+//               value={applicationStatus}
+//               onChange={(e) => setApplicationStatus(e.target.value)}
+//             >
+//               {applicationStatusOptions.map((status) => (
+//                 <option key={status} value={status}>
+//                   {status}
+//                 </option>
+//               ))}
+//             </select>
+//             <button onClick={updateApplicationStatus}>Update Status</button>
+//           </div>
+//         )}
 //         <div className="resume-content">{resumeContent}</div>
 //         <ul className="feed"></ul>
-//         <div className="bottom-section">
-//           <div className="input-container">
-//             <input />
-//             <div id="submit"> ➤</div>
-//           </div>
-//         </div>
+//         <div className="bottom-section"></div>
 //       </section>
 //     </div>
 //   );
@@ -134,6 +224,83 @@ const CustomizedResumePage = () => {
   const [resumeContent, setResumeContent] = useState("");
   const [jdFormHistory, setJdFormHistory] = useState([]);
   const { getAccessTokenSilently, user } = useAuth0();
+  const [selectedJobApplication, setSelectedJobApplication] = useState(null);
+  const [applicationStatus, setApplicationStatus] = useState({});
+  const applicationStatusOptions = [
+    "Draft",
+    "Submitted",
+    "Under Review",
+    "Interview",
+    "Offer Extended",
+    "Accepted Offer",
+    "Rejected",
+    "Withdrawn",
+  ];
+
+  // Function to fetch the current status
+  const fetchApplicationStatus = async (jdFormId) => {
+    if (!user || !jdFormId) {
+      console.error("User or jdFormId is undefined. Skipping fetch.");
+      return;
+    }
+    console.log(
+      `Fetching status for job application ID: ${jdFormId} and user: ${user.sub}`
+    );
+
+    try {
+      const statusResponse = await axios.get(
+        `${BACKEND_URL}/status/getStatus/${jdFormId}/${user.sub}`
+      );
+      console.log(`Status response:`, statusResponse);
+
+      if (statusResponse.status === 200) {
+        setApplicationStatus((prevStatus) => ({
+          ...prevStatus,
+          [jdFormId]: statusResponse.data.application.status,
+        }));
+      }
+    } catch (error) {
+      console.error("Error fetching status:", error);
+    }
+  };
+
+  // Function to update the status
+  const updateApplicationStatus = async () => {
+    if (!selectedJobApplication) return;
+
+    try {
+      const token = await getAccessTokenSilently();
+      await axios.post(
+        `${BACKEND_URL}/status/updateStatus`,
+        {
+          status: applicationStatus,
+          job_application_id: selectedJobApplication.id,
+          user_auth0_user_id: user.sub,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      // Update the application status in the jdFormHistory state
+      const updatedJdFormHistory = jdFormHistory.map((jdForm) => {
+        if (jdForm.id === selectedJobApplication.id) {
+          return {
+            ...jdForm,
+            status: applicationStatus[selectedJobApplication.id],
+          };
+        }
+        return jdForm;
+      });
+      setJdFormHistory(updatedJdFormHistory);
+
+      // Optionally, show a success message or handle the UI response
+    } catch (error) {
+      console.error("Error updating application status:", error);
+      // Handle the error case
+    }
+  };
 
   const fetchCustomizedResume = async ({ jdFormId }) => {
     if (!jdFormId) {
@@ -219,11 +386,17 @@ const CustomizedResumePage = () => {
           {jdFormHistory.map((jdForm) => (
             <li
               key={jdForm.id} // Using the 'id' property for the key
-              onClick={() => fetchCustomizedResume({ jdFormId: jdForm.id })}
+              onClick={() => {
+                fetchCustomizedResume({ jdFormId: jdForm.id });
+                fetchApplicationStatus(jdForm.id);
+                setSelectedJobApplication(jdForm);
+              }}
               style={{ cursor: "pointer" }}
             >
               <strong>Company:</strong> {jdForm.company_name} <br />
-              <strong>Title:</strong> {jdForm.job_title}
+              <strong>Title:</strong> {jdForm.job_title} <br />
+              <strong>Status:</strong>{" "}
+              {applicationStatus[jdForm.id] || "Not set"}
             </li>
           ))}
         </ul>
@@ -233,15 +406,25 @@ const CustomizedResumePage = () => {
         </nav>
       </section>
       <section className="main">
-        <h1>JOBWISE</h1>
+        {selectedJobApplication && (
+          <div>
+            <h2>Update Application Status</h2>
+            <select
+              value={applicationStatus}
+              onChange={(e) => setApplicationStatus(e.target.value)}
+            >
+              {applicationStatusOptions.map((status) => (
+                <option key={status} value={status}>
+                  {status}
+                </option>
+              ))}
+            </select>
+            <button onClick={updateApplicationStatus}>Update Status</button>
+          </div>
+        )}
         <div className="resume-content">{resumeContent}</div>
         <ul className="feed"></ul>
-        <div className="bottom-section">
-          <div className="input-container">
-            <input />
-            <div id="submit"> ➤</div>
-          </div>
-        </div>
+        <div className="bottom-section"></div>
       </section>
     </div>
   );
